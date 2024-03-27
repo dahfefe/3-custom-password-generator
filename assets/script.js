121c640281f628e065dd7d2af062e62dd89c08ab
Felix <157293782+dahfefe@users.noreply.github.com>
2024-03-27 01:02:58 -0700

    included code that allows first pop up alert requesting character count for password generator

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,24 @@ var passwordText = document.querySelector("#password");
 // Write password to the #password input
 function writePassword() {
 
+  var userNumberChoice = window.prompt("Enter the number of characters in your password (only 8 - 128 characters):");
+
+  // If user pressed Cancel, immediately end function
+  if (!userNumberChoice) {
+    return;
+  }
+
+  
+
+  // Function returns a string of concatenated characters of length num
+  function passwordGenerator(userNumberChoice) {
+  var password = '';
+  for (var i = 0; i < num; i++) {
+    password += getPasswordCharacter();
+  }
+  return password;
+}
+
   var newPassword = passwordGenerator(15);
   console.log(newPassword)
   // passwordText.text(newPassword);
@@ -17,15 +35,6 @@ function writePassword() {
 // Function returns a random character that includes alphanumeric and special character values
 function getPasswordCharacter() {
   return String.fromCharCode(Math.floor(Math.random() * 77) + 34);
-}
-
-// Function returns a string of concatenated characters of length num
-function passwordGenerator(num) {
-  var password = '';
-  for (var i = 0; i < num; i++) {
-    password += getPasswordCharacter();
-  }
-  return password;
 }
 
 // Add event listener to generate button
